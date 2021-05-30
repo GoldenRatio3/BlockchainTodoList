008de970f0e3e3bbf21e9fbdcf9f57c7ec6e1dc2
Brad Winter <bradleyswinter@hotmail.co.uk>
2021-05-30 12:02:50 +0100

    created UI

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 App = {
+  contracts: {},
+
   load: async () => {
     await App.loadWeb3()
     await App.loadAccount()
+    await App.loadContract()
+    await App.render()
   },
   
   loadWeb3: async () => {
@@ -38,8 +42,74 @@ App = {
 
   loadAccount: async () => {
     App.account = web3.eth.accounts[0]
-    console.log(App.account)
-  }
+  },
+
+  loadContract: async () => {
+    const todoList = await $.getJSON('TodoList.json')
+    App.contracts.TodoList = TruffleContract(todoList)
+    App.contracts.TodoList.setProvider(App.web3Provider)
+    
+    App.todoList = await App.contracts.TodoList.deployed()
+  },
+
+  render: async () => {
+    // prevent double render
+    if (App.loading) {
+      return
+    }
+
+    // update app loading state
+    App.setLoading(true)
+
+    // render account
+    $('#account').html(App.account)
+
+    // render tasks
+    await App.renderTasks()
+
+    // update loading state
+    App.setLoading(false)
+  },
+
+  renderTasks: async () => {
+    // load the tasks from the blockchain
+    const taskCount = await App.todoList.taskCount();
+    const $taskTemplate = $('.taskTemplate')
+
+    // render out each task with a new task template
+    for (var i = 1; i <= taskCount; i++) {
+      const task = await App.todoList.tasks(i)
+      const taskId = task[0].toNumber()
+      const taskContent = task[1]
+      const taskCompleted = task[2]
+
+      // create the new html for the task
+      const $newTaskTemplate = $taskTemplate.clone()
+      $newTaskTemplate.find('.content').html(taskContent)
+      $newTaskTemplate.find('input').prop('name', taskId).prop('checked', taskCompleted)
+      //.on('click', App.toggleCompleted)
+
+      if (taskCompleted) {
+        $('#completedTaskList').append($newTaskTemplate)
+      } else {
+        $('#taskList').append($newTaskTemplate)
+      }
+      $newTaskTemplate.show()
+    }    
+   },
+
+  setLoading: (boolean) => {
+    App.loading = boolean
+    const loader = $('#loader')
+    const content = $('#content')
+    if (boolean) {
+      loader.show()
+      content.hide()
+    } else {
+      loader.hide()
+      content.show()
+    }
+  } 
 }
 
 $(() => {
